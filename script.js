153154a772192dccc524016b16547e53876f90a5
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-09-25 11:05:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radiusInput = document.getElementById("radius").value.trim();
+	let radius = parseFloat(radiusInput);
+	let volume;
+	if(isNaN(radius) ||radius< 0){
+		volume = 0;
+	}
+	else{
+		volume = (4/3)* Math.PI * Math.pow(radius,3);
+		volume = volume.toFixed(4);
+	}
+	document.getElementById("volume").value = volume;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
